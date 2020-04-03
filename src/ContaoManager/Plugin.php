b84c3d164c31fe2218accdcaa728ed9a5a2d69be
Sebastian Zoglowek <zumklo@googlemail.com>
2020-04-03 17:08:09 +0200

    Initial Commit

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao ThemeManager RealEstate Plugin.
+ *
+ * (c) https://www.oveleon.de/
+ */
+
+namespace ContaoThemeManager\RealEstate\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use ContaoThemeManager\TinySlider\ContaoThemeManagerTinySlider;
+use ContaoThemeManager\RealEstate\ContaoThemeManagerRealEstate;
+use ContaoThemeManager\Core\ContaoThemeManagerCore;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(ContaoThemeManagerRealEstate::class)
+                ->setLoadAfter([ContaoCoreBundle::class, ContaoThemeManagerCore::class])
+                ->setReplace(['ctm-real-estate']),
+        ];
+    }
+}
---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * This file is part of Contao ThemeManager RealEstate Plugin.
+ *
+ * (c) https://www.oveleon.de/
+ */
+
+// Add SCSS sources
+$GLOBALS['TC_SOURCES']['configFiles'][] = 'bundles/ContaoThemeManagerRealEstate/framework/scss/_config.scss';
+$GLOBALS['TC_SOURCES']['files'][]       = 'bundles/ContaoThemeManagerRealEstate/framework/scss/_realestate.scss';
