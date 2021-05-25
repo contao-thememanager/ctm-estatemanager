46b2daade37d8a7b2df3533b21a525225be953a7
Sebastian Zoglowek <sebastian@oveleon.de>
2021-05-25 14:12:49 +0200

    [Update] Extended expose-modules with a second headline and the possibility to change their styles

--- src/Resources/contao/dca/tl_expose_module.php
+++ src/Resources/contao/dca/tl_expose_module.php
@@ -19,6 +19,9 @@ foreach ($GLOBALS['TL_DCA']['tl_expose_module']['palettes'] as $key=>$value){
     $palette->applyToPalette($key, 'tl_expose_module');
 }
 
+// Adding more headline options
+$GLOBALS['TL_DCA']['tl_expose_module']['fields']['headline']['options'] = array('h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'div');
+
 // Extend fields
 $GLOBALS['TL_DCA']['tl_expose_module']['fields']['styleManager'] = array
 (
@@ -29,5 +32,37 @@ $GLOBALS['TL_DCA']['tl_expose_module']['fields']['styleManager'] = array
     'sql'                     => "blob NULL"
 );
 
+$GLOBALS['TL_DCA']['tl_expose_module']['fields']['headlineStyle'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_expose_module']['headlineStyle'],
+	'exclude'                 => true,
+	'inputType'               => 'select',
+	'options'                 => array('h1', 'h2', 'h3', 'h4', 'h5', 'h6'),
+	'eval'                    => array('includeBlankOption'=>true, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(2) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_expose_module']['fields']['headline2'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_expose_module']['headline2'],
+	'exclude'                 => true,
+	'search'                  => true,
+	'inputType'               => 'inputUnit',
+	'options'                 => array('h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'div'),
+	'eval'                    => array('tl_class'=>'w50 clr'),
+	'sql'                     => "mediumtext NULL"
+);
+
+$GLOBALS['TL_DCA']['tl_expose_module']['fields']['headline2Style'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_expose_module']['headline2Style'],
+	'exclude'                 => true,
+	'inputType'               => 'select',
+	'options'                 => array('h1', 'h2', 'h3', 'h4', 'h5', 'h6'),
+	'eval'                    => array('includeBlankOption'=>true, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(2) NOT NULL default ''"
+);
+
 $GLOBALS['TL_DCA']['tl_expose_module']['fields']['cssID']['load_callback'][] = array('\\Oveleon\\ContaoComponentStyleManager\\StyleManager', 'onLoad');
 $GLOBALS['TL_DCA']['tl_expose_module']['fields']['cssID']['save_callback'][] = array('\\Oveleon\\ContaoComponentStyleManager\\StyleManager', 'onSave');
+$GLOBALS['TL_DCA']['tl_expose_module']['config']['onload_callback'][] = array('ContaoThemeManager\Core\ThemeManager', 'extendHeadlineField');
